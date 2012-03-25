0f213ae19bd0efa934c62a63f4d7e5e6e7770db7
Kurtis LoVerde <github@loverde.org>
2012-03-25 18:42:09 -0400

    libwiigui:  Added author credits to the new headers

--- source/gui/gui_tooltip.h
+++ source/gui/gui_tooltip.h
@@ -1,3 +1,39 @@
+/*!\mainpage libwiigui Documentation
+ *
+ * \section Introduction
+ * libwiigui is a GUI library for the Wii, created to help structure the
+ * design of a complicated GUI interface, and to enable an author to create
+ * a sophisticated, feature-rich GUI. It was originally conceived and written
+ * after I started to design a GUI for Snes9x GX, and found libwiisprite and
+ * GRRLIB inadequate for the purpose. It uses GX for drawing, and makes use
+ * of PNGU for displaying images and FreeTypeGX for text. It was designed to
+ * be flexible and is easy to modify - don't be afraid to change the way it
+ * works or expand it to suit your GUI's purposes! If you do, and you think
+ * your changes might benefit others, please share them so they might be
+ * added to the project!
+ *
+ * \section Quickstart
+ * Start from the supplied template example. For more advanced uses, see the
+ * source code for Snes9x GX, FCE Ultra GX, and Visual Boy Advance GX.
+ *
+ * \section Contact
+ * If you have any suggestions for the library or documentation, or want to
+ * contribute, please visit the libwiigui website:
+ * http://code.google.com/p/libwiigui/
+ *
+ * \section Credits
+ * This library was wholly designed and written by Tantric. Thanks to the
+ * authors of PNGU and FreeTypeGX, of which this library makes use. Thanks
+ * also to the authors of GRRLIB and libwiisprite for laying the foundations.
+ *
+ *
+ * History:
+ *
+ * Name           Date     Description
+ * ----------  mm/dd/yyyy  --------------------------------------------------
+ * kloverde    03/25/2012  New file (moved out of gui.h)
+ */
+
 #ifndef _GUI_TOOLTIP_H_
 #define _GUI_TOOLTIP_H_
 
